271cac7707ab7824290e1d4c3fe44eb62e0168a7
Pranish Pantha <panthapranish4@gmail.com>
2022-06-16 10:29:27 -0400

    week 2

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,31 +1,32 @@
 import React, { Component } from "react";
 import "./About.css";
-import profile_pic from "../assets/profile_pic.jpg";
+import profile_pic from "../assets/pranish.png";
 
 export default class About extends Component {
   render() {
     return (
       <div>
-        {/* <p>Design your About me page </p> */}
-        <div class="split left">
-          <div className="centered">
-            <img
-              className="profile_image"
-              src={profile_pic}
-              alt="Profile Pic"
-            ></img>
-          </div>
-        </div>
-        <div className="split right">
-          <div className="centered">
-            <div className="name_title">Your Name</div>
-            <div className="brief_description">
-              Tell us about yourself in a few sentences. Tell us your interests
-              and say a fun fact about yourself.
-            </div>
-          </div>
+         <div>
+    <div class="split left">
+      <div className="centered">
+        <img 
+          className="profile_image"
+          // Image goes here
+          alt="Profile Pic"
+          src={profile_pic}
+          ></img>
+      </div>
+    </div>
+    <div className="split right">
+      <div className="centered">
+        <div className="name_title">Pranish Pantha</div>
+        <div className="brief_description">
+          Student at UNC Chapel Hill Studying Computer Science and Business
         </div>
       </div>
-    );
+    </div>
+  </div>
+      </div>
+    )
   }
 }
